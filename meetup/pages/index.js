503b9a75ea31f1479edf14878a4b998a40b8877a
eyalItzhak <62293316+eyalItzhak@users.noreply.github.com>
2022-04-21 13:31:37 +0300

    getStaticPorps

--- meetup/pages/index.js
+++ meetup/pages/index.js
@@ -1,30 +1,36 @@
 import MeetupList from '../components/meetups/MeetupList';
 
 
-
 const DUMMY_MEETUPS = [
-    {
-      id: 'm1',
-      title: 'A First Meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Some address 5, 12345 Some City',
-      description: 'This is a first meetup!'
-    },
-    {
-      id: 'm2',
-      title: 'A Second Meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Some address 10, 12345 Some City',
-      description: 'This is a second meetup!'
-    }
-  ];
-  
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!',
+  },
+];
 
-
-function homePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
-export default homePage;
 
+export async function getStaticProps(){ //pre render
+      return {
+        props:{
+          meetups:DUMMY_MEETUPS,
+        },
+        revalidate: 10 //page reset every 10 sec
+      };
+}
 
-
+export default HomePage;
